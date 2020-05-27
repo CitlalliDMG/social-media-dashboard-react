e28cf60199d30a908bb3282e95f7ca18bff2b4a4
Citlalli DMG <citlallidmg@gmail.com>
2020-05-27 00:19:54 -0500

    Updates

---
+++ precache-manifest.deffa1acd2b179642fddf8ff31f79e76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5147aefa55c24461a565d68da9a7e6aa",
+    "url": "/social-media-dashboard-react/index.html"
+  },
+  {
+    "revision": "391b677ddfec86cb1d24",
+    "url": "/social-media-dashboard-react/static/css/main.0ec35b15.chunk.css"
+  },
+  {
+    "revision": "8ef732155a7fd87cef38",
+    "url": "/social-media-dashboard-react/static/js/2.a216194d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/social-media-dashboard-react/static/js/2.a216194d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "391b677ddfec86cb1d24",
+    "url": "/social-media-dashboard-react/static/js/main.ea6ff378.chunk.js"
+  },
+  {
+    "revision": "eecbb8edadf1ae9e4e2b",
+    "url": "/social-media-dashboard-react/static/js/runtime-main.647d518f.js"
+  }
+]);
